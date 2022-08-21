a94903ab7c3364cc29edab71022feea8cdd23499
Kuldeep Saini <cool.kuldeep344@gmail.com>
2022-08-22 01:38:38 +0530

    Update 2

--- app/src/main/java/com/example/toonimage/MainActivity.java
+++ app/src/main/java/com/example/toonimage/MainActivity.java
@@ -1,115 +1,70 @@
 package com.example.toonimage;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 
-import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
-    public static final int CAMERA_PERM_CODE = 101;
-    public static final int CAMERA_REQUEST_CODE = 102;
+public class git    MainActivity extends AppCompatActivity {
     public static final int GALLERY_REQUEST_CODE = 105;
     ImageView selectedImage;
-    Button cameraBtn, toonBtn;
-    String currentPhotoPath;
-
-
+    Button toonBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         selectedImage = findViewById(R.id.displayImageView);
-        cameraBtn = findViewById(R.id.cameraBtn);
         toonBtn = findViewById(R.id.toonBtn);
 
-        cameraBtn.setOnClickListener(view -> askCameraPermission());
-
-        toonBtn.setOnClickListener((View view) -> {
+        toonBtn.setOnClickListener(view -> {
             Intent gallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(gallery, GALLERY_REQUEST_CODE);
         });
-
     }
 
-    private void askCameraPermission() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
-        }else {
-            dispatchTakePictureIntent();
-        }
-    }
-    @SuppressLint("MissingSuperCall")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        /* super.onRequestPermissionsResult(requestCode, permissions, grantResults); */
-        if (requestCode == CAMERA_PERM_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                dispatchTakePictureIntent();
-            } else {
-                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @SuppressLint("MissingSuperCall")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        /* super.onActivityResult(requestCode, resultCode, data); */
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            if(resultCode == Activity.RESULT_OK){
-                File f = new File(currentPhotoPath);
-                selectedImage.setImageURI(Uri.fromFile(f));
-                Log.d("tag", "Absolute URL of File is "+Uri.fromFile(f));
-
-                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                Uri contentUri = Uri.fromFile(f);
-                mediaScanIntent.setData(contentUri);
-                this.sendBroadcast(mediaScanIntent);
-            }
-        }
-        if(requestCode == GALLERY_REQUEST_CODE) if (resultCode == Activity.RESULT_OK) {
-            assert data != null;
-            Uri contentUri = data.getData();
-            @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
-            Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
-            selectedImage.setImageURI(contentUri);
-            try {
-                Bitmap src = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentUri);
-                new Handler().postDelayed(() -> selectedImage.setImageBitmap(toonImage(src, 128)), 5000);
-            } catch (IOException e) {
-                e.printStackTrace();
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK) {
+                assert data != null;
+                Uri contentUri = data.getData();
+                @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
+                selectedImage.setImageURI(contentUri);
+                try {
+                    Bitmap src = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentUri);
+                    new Handler().postDelayed(() -> selectedImage.setImageBitmap(toonImage(src, 85)), 2000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
     public static Bitmap toonImage(Bitmap src, int bitOffset) {
         int width = src.getWidth();
         int height = src.getHeight();
@@ -118,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
         // color information
         int A, R, G, B;
         int pixel;
-
         // scan through all pixels
         for(int x = 0; x < width; ++x) {
             for(int y = 0; y < height; ++y) {
@@ -128,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 R = Color.red(pixel);
                 G = Color.green(pixel);
                 B = Color.blue(pixel);
-
                 // round-off color offset
                 R = ((R + (bitOffset / 2)) - ((R + (bitOffset / 2)) % bitOffset) - 1);
                 if(R < 0) { R = 0; }
@@ -136,59 +89,11 @@ public class MainActivity extends AppCompatActivity {
                 if(G < 0) { G = 0; }
                 B = ((B + (bitOffset / 2)) - ((B + (bitOffset / 2)) % bitOffset) - 1);
                 if(B < 0) { B = 0; }
-
                 // set pixel color to output bitmap
                 bmOut.setPixel(x, y, Color.argb(A, R, G, B));
             }
         }
-
         // return final image
         return bmOut;
-    }
-
-    private String getFileExt(Uri contentUri) {
-        ContentResolver c = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(c.getType(contentUri));
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ignored) {
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-            }
-        }
     }
 }
